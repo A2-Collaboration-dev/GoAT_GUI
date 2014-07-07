6cada7efed854f07164730ac71e97ccf570b2bbc
Augustinas Malinauskas <augustinasmal@gmail.com>
2014-07-07 14:31:36 +0200

    Comment added

--- ConfigurationDialog.cpp
+++ ConfigurationDialog.cpp
@@ -501,6 +501,7 @@ void ConfigurationDialog::updateUIText()
     {
     // Cuts
     ui->toolBox->setItemEnabled(1, true);
+    //comment here
     ui->labelPRProtons->setText(QString(configFile.getCut_dE_E_CB_Proton().c_str()));
     ui->labelPRPions->setText(QString(configFile.getCut_dE_E_CB_Pion().c_str()));
     ui->labelPRElectrons->setText(QString(configFile.getCut_dE_E_CB_Electron().c_str()));
